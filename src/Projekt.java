c17a4ed3d437a63accf0f15cebe28507ceaf6427
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-05-21 19:52:05 +0200

    dodanie konstruktora do klasy Zwierze

--- src/Projekt.java
+++ src/Projekt.java
@@ -1,28 +1,15 @@
 public class Projekt {
 
     public static void main(String[] args) {
-        Zwierze zwierze1 = new Zwierze();
-        zwierze1.imie = "Filip";
-           zwierze1.rasa ="Brytyjski";
-            zwierze1.płeć ="KOT";
-            zwierze1.wiek =2;
-            zwierze1.umaszczenie ="srebrzysty";
-            zwierze1.waga = 5.0;
-            zwierze1.charakter ="agresywny";
+        Zwierze zwierze1 = new Zwierze("Brytyjski", 5, "Kot", "Filip", "srebrzysty", 5.0, "agresywny");
 
 
-        Zwierze zwierze2 = new Zwierze();
-            zwierze2.imie = "Danwir";
-            zwierze2.rasa = "Syberysjki";
-            zwierze2.płeć = "Kot";
-            zwierze2.wiek = 2;
-            zwierze2.umaszczenie = "bialy";
-            zwierze2.waga = 6;
-            zwierze2.charakter = "łagodny";
+
+        Zwierze zwierze2 = new Zwierze( "Syberyjski", 3,"Kot", "Danvir", "biały", 6.0,  "łagodny");
 
         System.out.println(zwierze1.imie);
         System.out.println(zwierze1.rasa);
-        System.out.println(zwierze1.płeć);
+        System.out.println(zwierze1.plec);
         System.out.println(zwierze1.wiek);
         System.out.println(zwierze1.umaszczenie);
         System.out.println(zwierze1.waga);
@@ -30,7 +17,7 @@ public class Projekt {
 
         System.out.println(zwierze2.imie);
         System.out.println(zwierze2.rasa);
-        System.out.println(zwierze2.płeć);
+        System.out.println(zwierze2.plec);
         System.out.println(zwierze2.wiek);
         System.out.println(zwierze2.umaszczenie);
         System.out.println(zwierze2.waga);
--- src/Zwierze.java
+++ src/Zwierze.java
@@ -2,9 +2,23 @@ class Zwierze {
 
     String rasa;
     int wiek;
-    String płeć;
+    String plec;
     String imie;
     String umaszczenie;
     double waga;
     String charakter;
+
+    Zwierze(String r, int w, String pl, String i, String u, double wa, String ch)
+
+    {
+        rasa = r;
+        wiek = w;
+        plec = pl;
+        imie = i;
+        umaszczenie = u;
+        waga = wa;
+        charakter = ch;
+    }
+
+
 }
