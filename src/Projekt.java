f5bb651f2f304a52408bae57a8c9c4ed556eb7c3
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-05-21 19:33:17 +0200

    2 koty w worku

---
+++ src/Projekt.java
@@ -0,0 +1,41 @@
+public class Projekt {
+
+    public static void main(String[] args) {
+        Zwierze zwierze1 = new Zwierze();
+        zwierze1.imie = "Filip";
+           zwierze1.rasa ="Brytyjski";
+            zwierze1.płeć ="KOT";
+            zwierze1.wiek =2;
+            zwierze1.umaszczenie ="srebrzysty";
+            zwierze1.waga = 5.0;
+            zwierze1.charakter ="agresywny";
+
+
+        Zwierze zwierze2 = new Zwierze();
+            zwierze2.imie = "Danwir";
+            zwierze2.rasa = "Syberysjki";
+            zwierze2.płeć = "Kot";
+            zwierze2.wiek = 2;
+            zwierze2.umaszczenie = "bialy";
+            zwierze2.waga = 6;
+            zwierze2.charakter = "łagodny";
+
+        System.out.println(zwierze1.imie);
+        System.out.println(zwierze1.rasa);
+        System.out.println(zwierze1.płeć);
+        System.out.println(zwierze1.wiek);
+        System.out.println(zwierze1.umaszczenie);
+        System.out.println(zwierze1.waga);
+        System.out.println(zwierze1.charakter);
+
+        System.out.println(zwierze2.imie);
+        System.out.println(zwierze2.rasa);
+        System.out.println(zwierze2.płeć);
+        System.out.println(zwierze2.wiek);
+        System.out.println(zwierze2.umaszczenie);
+        System.out.println(zwierze2.waga);
+        System.out.println(zwierze2.charakter);
+
+
+
+}}
---
+++ src/Zwierze.java
@@ -0,0 +1,10 @@
+class Zwierze {
+
+    String rasa;
+    int wiek;
+    String płeć;
+    String imie;
+    String umaszczenie;
+    double waga;
+    String charakter;
+}
